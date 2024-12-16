e2882308530c573463aabc64fbfd2f95c53bf3d1
nivek <literalmentebruno@gmail.com>
2024-12-16 16:53:10 -0300

    all-pokemons

--- pokemon.js
+++ pokemon.js
@@ -1,5 +1,5 @@
 const MAX_POKEMON = 151;
-const listWrapper = document.querySelector(".list-wrapper");
+const listWrapper = document.querySelector('.list-wrapper');
 const numberFilter = document.querySelector('#number');
 const nameFilter = document.querySelector('#name');
 const notFoundMessage = document.querySelector('#not-found-message');
@@ -7,21 +7,23 @@ const searchInput = document.querySelector('#search-input');
 
 let allPokemons = [];
 
-fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`).then((response)=> response.json())
-.then((data)=> {
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
+  .then((response) => response.json())
+  .then((data) => {
     allPokemons = data.results;
-});
-async function fetchPokemonDataBeforeRedirect(id){
- try{
-    const [pokemon, pokemonSpecies] = await Promise.all([fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res)=>
-        res.json()
-    ),
-    fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res)=>
-        res.json()
-    ),
-])
-    return true
- } catch(error){
-    console.error('failed to fetch Pokemon data before redirect')
- }
+  });
+async function fetchPokemonDataBeforeRedirect(id) {
+  try {
+    const [pokemon, pokemonSpecies] = await Promise.all([
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
+        res.json(),
+      ),
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
+        res.json(),
+      ),
+    ]);
+    return true;
+  } catch (error) {
+    console.error('failed to fetch Pokemon data before redirect');
+  }
 }
