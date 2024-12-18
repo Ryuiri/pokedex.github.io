ba39c67a30a73bb1f3a75b1542fdf9e6d4ec5929
nivek <literalmentebruno@gmail.com>
2024-12-18 16:00:06 -0300

    detail.js

--- pokemon.js
+++ pokemon.js
@@ -1,9 +1,9 @@
 const MAX_POKEMON = 151;
-const listWrapper = document.querySelector('.list-wrapper');
-const numberFilter = document.querySelector('#number');
-const nameFilter = document.querySelector('#name');
-const notFoundMessage = document.querySelector('#not-found-message');
-const searchInput = document.querySelector('#search-input');
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
 
 let allPokemons = [];
 
@@ -13,50 +13,59 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
     allPokemons = data.results;
     displayPokemons(allPokemons);
   });
+
 async function fetchPokemonDataBeforeRedirect(id) {
   try {
     const [pokemon, pokemonSpecies] = await Promise.all([
       fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
-        res.json(),
+        res.json()
       ),
       fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
-        res.json(),
+        res.json()
       ),
     ]);
     return true;
   } catch (error) {
-    console.error('failed to fetch Pokemon data before redirect');
+    console.error("Failed to fetch Pokemon data before redirect");
   }
 }
+
 function displayPokemons(pokemon) {
-  listWrapper.innerHTML = '';
+  listWrapper.innerHTML = "";
+
   pokemon.forEach((pokemon) => {
-    const pokemonID = pokemon.url.split('/')[6];
-    const listItem = document.createElement('div');
-    listItem.className = 'list-item';
-    listItem.innerHTML = `<div class="number-wrap">
-          <p class="caption-fonts">#${pokemonID}</p>
-    </div>
-   <div class="img-wrap">
-         <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemon.name}"/>
-    </div>
-    <div class="name-wrap">
-          <p class="body3-fonts">${pokemon.name}</p>
-    </div>`;
-    listItem.addEventListener('click', async () => {
+    const pokemonID = pokemon.url.split("/")[6];
+    const listItem = document.createElement("div");
+    listItem.className = "list-item";
+    listItem.innerHTML = `
+        <div class="number-wrap">
+            <p class="caption-fonts">#${pokemonID}</p>
+        </div>
+        <div class="img-wrap">
+            <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemon.name}" />
+        </div>
+        <div class="name-wrap">
+            <p class="body3-fonts">#${pokemon.name}</p>
+        </div>
+    `;
+
+    listItem.addEventListener("click", async () => {
       const success = await fetchPokemonDataBeforeRedirect(pokemonID);
       if (success) {
         window.location.href = `./detail.html?id=${pokemonID}`;
       }
     });
+
     listWrapper.appendChild(listItem);
   });
 }
-searchInput.addEventListener('keyup', handleSearch);
+
+searchInput.addEventListener("keyup", handleSearch);
+
 function handleSearch() {
   const searchTerm = searchInput.value.toLowerCase();
-
   let filteredPokemons;
+
   if (numberFilter.checked) {
     filteredPokemons = allPokemons.filter((pokemon) => {
       const pokemonID = pokemon.url.split("/")[6];
@@ -71,17 +80,19 @@ function handleSearch() {
   }
 
   displayPokemons(filteredPokemons);
-  if (filteredPokemons.length === 0) {
-    notFoundMessage.style.display = "block"
-  } else {
-    notFoundMessage.style.display = "none"
-  }
 
+  if (filteredPokemons.length === 0) {
+    notFoundMessage.style.display = "block";
+  } else {
+    notFoundMessage.style.display = "none";
+  }
 }
- const closeButton = document.querySelector(".search-close-icon");
- closeButton.addEventListener("click", clearSearch);
- function clearSearch(){
+
+const closeButton = document.querySelector(".search-close-icon");
+closeButton.addEventListener("click", clearSearch);
+
+function clearSearch() {
   searchInput.value = "";
   displayPokemons(allPokemons);
-  notFoundMessage.style.display = "none"
- }
+  notFoundMessage.style.display = "none";
+}
